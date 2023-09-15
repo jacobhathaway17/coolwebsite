e1257eb6d52259d22bf978c965d7d07bc598bde0
jacobhathaway17 <119406903+jacobhathaway17@users.noreply.github.com>
2023-09-15 09:24:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function sendMessage() {
-    var messageInput = document.getElementById("message-input");
-    var messageContainer = document.getElementById("message-container");
+document.addEventListener('DOMContentLoaded', function() {
+    const output = document.getElementById('output');
+    const choice1 = document.getElementById('choice1');
+    const choice2 = document.getElementById('choice2');
 
-    var messageText = messageInput.value.trim();
+    choice1.addEventListener('click', function() {
+        output.textContent = "You chose Choice 1. What happens next?";
+        choice1.textContent = "Continue to Choice A";
+        choice2.textContent = "Continue to Choice B";
+    });
 
-    if (messageText !== "") {
-        var messageElement = document.createElement("div");
-        messageElement.classList.add("message");
-        messageElement.innerText = messageText;
-
-        messageContainer.appendChild(messageElement);
-
-        messageInput.value = "";
-        messageContainer.scrollTop = messageContainer.scrollHeight;
-    }
-}
+    choice2.addEventListener('click', function() {
+        output.textContent = "You chose Choice 2. What happens next?";
+        choice1.textContent = "Continue to Choice X";
+        choice2.textContent = "Continue to Choice Y";
+    });
+});
